5b66fe86ded95793a527dc23b2c6b16906756745
Roman <gsmartrsh@gmail.com>
2018-09-18 18:02:49 +0300

    Card and character concept

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardTypes
+{
+    War,
+    Arc,
+    Mag
+}
+
+public class Card : MonoBehaviour {
+
+    public string Name;
+    public Sprite Icon;
+    public int Level;
+    public int Exp;
+    public int HitPoints;
+
+    public int Atack;
+    public int Armor;
+    public int MagicDef;
+    public int Speed;
+
+    public CardTypes CardType;
+
+    public List<Skill> Skills;
+}
--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -6,17 +6,13 @@ public class Character : MonoBehaviour
 {
     public string Name;
     public Sprite Icon;
-    public int hitPointsMax;
-    public int hitPointsCurent;
-
-    public int manaPointsMax;
-    public int manaPointsCurent;
+    public int HitPointsMax;
+    public int HitPointsCurent;
 
     public int atack;
     public int armor;
-    public int magicPower;
+    public int magicDef;
     public int speed;
 
-    public List<Spell> Spells;
     public List<Skill> Skills;
 }
